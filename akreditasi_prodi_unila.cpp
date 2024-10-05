90148cc812afdd0289fb937a844e1e30287702e4
karinaaftrr <karinafitriamalia05@gmail.com>
2024-10-06 00:46:24 +0700

    Update akreditasi_prodi_unila.cpp

--- akreditasi_prodi_unila.cpp
+++ akreditasi_prodi_unila.cpp
@@ -1,5 +1,47 @@
-#include<iostream
+#include<string>
 using namespace std;
 
-int main () {
-}
+struct Akreditasi {
+	string NamaFakultas;
+	string NamaJurusan;
+	string NamaProdi;
+	string Akreditasi;
+	string TanggalBerlaku;
+    };
+void TampilkanAkreditasi(int Fakultas, int Jurusan) {
+	Akreditasi akreditasiData[] = {
+		{"FMIPA", "Ilmu Komputer", "D3 Manajemen Informatika", "Baik Sekali"; "27-12-2027"};
+	    {"FMIPA", "Ilmu Komputer", "S1 Sistem Informasi", "Proses/Prodi Baru"; "-"};
+	    {"FMIPA", "Ilmu Komputer", "S1 Ilmu Komputer", "A"; "25-11-2026"};
+	    {"FMIPA", "Ilmu Komputer", "Ilmu Komputer PSDKU Way Kanan", "Proses/Prodi Baru"; "-"};
+	    {"FMIPA", "Matematika", "S1 Matematika", "B"; "14-07-2025"};
+	    {"FMIPA", "Fisika", "S1 Fisika", "A"; "9-10-2024"};
+	    {"FMIPA", "Biologi", "S1 Biologi", "A"; "06-02-2026"};
+	    {"FMIPA", "Biologi Terapan", "s!", "Baik"; "21-09-2026"};
+	    {"FMIPA", "Kimia", "S1 Kimia", "A"; "11-01-2027"};
+	    {"FKIP", "Ilmu Pendidikan", "S1 Pendidikan Bimbingan dan Konseling", "B"; "26-05-2025"};
+	    {"FKIP", "Ilmu Pendidikan", "S1 Pendidikan Teknologi Informasi", "Baik"; "22-12-2026"};
+	    {"FKIP", "Ilmu Pendidikan", "S1 PGSD-D", "Baik Sekali"; "22-08-2027"};
+	    {"FKIP", "Ilmu Pendidikan", "S1 PGSD", "Unggul"; "31-07-2028"};
+	    {"FKIP", "Ilmu Pendidikan", "S1 Pendidikan Jasmani", "Baik Sekali"; "15-02-2027"};
+	    {"FKIP", "Ilmu Pendidikan", "S1 Pendidikan Jasmani", "Baik Sekali"; "15-02-2027"};
+	    {"FKIP", "Pendindikan Bahasa dan Seni", "S1 Pendindikan Bahasa Indonesia dan Sastra Indonesia", "Unggul"; "12-12-2028"};
+	    {"FKIP", "Pendindikan Bahasa dan Seni", "S1 Pendidikan Bahasa Inggris", "Baik Sekali"; "20-04-2027"};
+	    {"FKIP", "Pendindikan Bahasa dan Seni", "S1 Pendidikan Bahasa Lampung", "Proses/Prodi Baru"; "-"};
+	    {"FKIP", "Pendindikan Bahasa dan Seni", "S1 Pendidikan Perancis", "Baik Sekali"; "09-10-2028"};
+	    {"FKIP", "Pendindikan Bahasa dan Seni", "S1 Pendidikan Biologi", "B"; "16-08-2025"};
+	    {"FKIP", "Pendindikan Bahasa dan Seni", "S1 Pendidikan Musik", "Baik"; "04-05-2026"};
+	    {"FKIP", "Pendidikan Bahasa dan Seni", "S1 Pendidikan Tari", "B"; "14-04-2025"};
+	    {"FKIP", "Pendidikan IPS", "S1 Pendidikan Ekonomi", "B"; "09-10-14"};
+	    {"FKIP", "Pendidikan IPS", "S1 Pendidikan Geografi", "Unggul"; "20-12-2028"};
+	    {"FKIP", "Pendidikan IPS", "S1 Pendidikan Sejarah", "B"; "11-03-2025"};
+	    {"FKIP", "Pendidikan IPS", "S1 PPKN", "B"; "17-11-2025"};
+	    {"FKIP", "Pendidikan MIPA", "S1 Pendidikan Fisika", "A"; "10-11-2025"};
+	    {"FKIP", "Pendidikan MIPA", "S1 Pendidikan Kimia", "Baik Sekali"; "12-12-2028"};
+	    {"FKIP", "Pendidikan MIPA", "S1 Pendidikan Matematika", "b"; "11-03-2025"};
+	
+	
+	    
+		}
+	}
+
